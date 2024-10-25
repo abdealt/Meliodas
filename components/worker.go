b012b7aaa97b732f26557ee57c621e08db7b25c0
abdealt <abdenour.allaouat2@gmail.com>
2024-10-25 15:11:06 +0200

    Apport correction de fonctionnement

---
+++ components/worker.go
@@ -0,0 +1,34 @@
+package components
+
+import "fmt"
+
+type Config struct {
+	Lst_Dprt      []string
+	Lst_Insee     []string
+	File_immeuble string
+	File_export   string
+	File_log      string
+}
+
+type WorkerImmeuble struct {
+	Config *Config
+}
+
+func NewWorkerImmeuble(cfg Config) (*WorkerImmeuble, error) {
+	workerImmeuble := &WorkerImmeuble{
+		Config: &cfg,
+	}
+	return workerImmeuble, nil
+}
+
+// SynoReaderRunner est la fonction d'execution des traitements autour du synoptique
+func (wi *WorkerImmeuble) SuperreaderCSV() error {
+	fmt.Println(wi.Config.File_export)
+
+	// Faire le traitement de lecture du fichier IMMEUBLE
+	// Lire les parametres de config depuis wi.Config
+
+	// Exporter les infos vers le CSV
+
+	return nil
+}
