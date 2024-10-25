6390312e0a837e682b5abaf28e1eee14f97770d2
abdealt <abdenour.allaouat2@gmail.com>
2024-10-25 09:21:08 +0200

    Finitions

--- components/ReadCSVFileContentAndExtracter.go
+++ components/ReadCSVFileContentAndExtracter.go
@@ -19,26 +19,24 @@ var DepartID string
 var ComptTotal int
 var ComptElement int
 var Stat string
+var codeDept string
 
 func ReadCSVFileContentAndExtracter() {
 	// On initialise le temps qui servira plutard pour l'horodatage du fichier exporté
 	now := time.Now()
 
-	// Charger les variables d'environnement à partir du fichier .env -- le chargement des variables est commenté car le fonctionnement a changé, les variable ont chargé depuis le CLI
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	fmt.Printf("Erreur lors du chargement du fichier .env : %v\n", err)
-	// 	return
-	// }
+	/*
+		Les variables du fichier .env sont chargé grace à cmd/root.go (depuis le projet du CLI)
+	*/
 
-	// Récupérer le chemin du fichier CSV (SOURCE) depuis les variables d'environnement
+	// Récupéreration le chemin du fichier CSV (SOURCE) depuis les variables d'environnement
 	FilePath = os.Getenv("SOURCE_FILE")
 	if FilePath == "" {
 		fmt.Printf("Aucun chemin de fichier fourni dans le fichier de configuration .env\n")
 		return
 	}
 
-	// Récupérer la variable CITY_INSEE ou DEPARTMENT_ID depuis le fichier .env
+	// Récupéreration la variable CITY_INSEE ou DEPARTMENT_ID depuis le fichier .env
 	CityINSEE = os.Getenv("CITY_INSEE")
 	DepartID = os.Getenv("DEPARTMENT_ID")
 	if CityINSEE == "" && DepartID == "" {
@@ -50,34 +48,40 @@ func ReadCSVFileContentAndExtracter() {
 	DepartList := strings.Split(DepartID, ",")
 	InseeList := strings.Split(CityINSEE, ",")
 
-	// Récupérer le chemin du fichier d'extraction depuis les variables d'environnement
+	// Récupérer le chemin d'extraction depuis les variables d'environnement (La ou sera extrait le fichier)
 	ExtractFilePath := os.Getenv("EXTRACT_FILE")
 	if ExtractFilePath == "" {
 		fmt.Printf("Aucun chemin pour le fichier d'extraction (EXTRACT_FILE) n'est fourni dans le fichier de configuration .env\n")
 		return
 	}
 
-	// Ouvrir le fichier CSV (Source)
+	// Ouvrerture du fichier CSV (Source)
 	csvFile, err := os.Open(FilePath)
 	if err != nil {
 		fmt.Printf("Erreur survenue lors de l'ouverture du fichier Source : %v\n", err)
 		return
 	}
-	// On s'assur de sa fermeture a la fin de la fonction
+	// On s'assure de sa fermeture a la fin de la fonction
 	defer csvFile.Close()
 
 	// Création d'un reader pour lire le fichier source
 	r := csv.NewReader(csvFile)
-	r.Comma = ',' // Définit le séparateur du fichier source
+	// Définit le séparateur du reader du fichier source
+	r.Comma = ','
 
-	// Déterminer le nom complet du fichier d'extraction
+	// Création du nom du fichier d'extractions
 	if CityINSEE == "" {
 		CompleteExtractFileName = "Extraction_du_" + now.Format("2006-01-02_15-04-05") + "_PAR_DPT_" + DepartID
 	} else {
 		CompleteExtractFileName = "Extraction_du_" + now.Format("2006-01-02_15-04-05") + "_PAR_INSEE_" + CityINSEE
 	}
 
-	// Créer le fichier d'extraction
+	// Si les deux variable ne sont pas vides
+	if CityINSEE != "" && DepartID != "" {
+		CompleteExtractFileName = "Extraction_du_" + now.Format("2006-01-02_15-04-05") + "_PAR_INSEE_" + CityINSEE + "_PAR_DPT_" + DepartID
+	}
+
+	// Création du fichier d'extraction
 	csvExtractedFile, err := os.Create(ExtractFilePath + CompleteExtractFileName + ".csv")
 	if err != nil {
 		fmt.Printf("Erreur survenue lors de l'ouverture du fichier Extraction : %v\n", err)
@@ -88,7 +92,7 @@ func ReadCSVFileContentAndExtracter() {
 
 	// Créer un writer pour écrire dans le fichier d'extraction
 	w := csv.NewWriter(csvExtractedFile)
-	// Définir le séparateur du fichier d'extraction
+	// Définir le séparateur pour le writer du fichier d'extraction
 	w.Comma = ';'
 
 	// On s'assure que ce qui doit etre écrit est ecris a la fin de la fonction
@@ -98,31 +102,35 @@ func ReadCSVFileContentAndExtracter() {
 	header := []string{"x", "y", "imb_id", "num_voie", "cp_no_voie", "type_voie", "nom_voie", "batiment", "code_insee", "code_poste", "nom_com", "catg_loc_imb", "imb_etat", "pm_ref", "pm_etat", "code_l331", "geom_mod", "type_imb"}
 	w.Write(header)
 
-	var codeDept string
-
-	// Lire et traiter les enregistrements du fichier source
+	// Boucle pour lire et traiter chaque enregistrement
 	for {
+		// Incrémentation du compteur du total d'élements
 		ComptTotal++
+
 		// Lire une ligne du fichier source
 		record, err := r.Read()
 
-		// Vérifier si on a atteint la fin du fichier
+		// Si l'érreur est la fin du fichier alors on arrête
 		if err == io.EOF {
 			break
 		}
 
-		// Gestion des erreurs de lecture
+		// Si un erreur est rencontrait on continue quand même
 		if err != nil {
-			fmt.Printf("Erreur lors de la lecture d'une ligne : %v\n", err)
+			fmt.Printf("Erreur lors de la lecture d'une ligne suivante : %v\n", err)
 			continue
 		}
 
-		// Vérifier si la ligne contient suffisamment de colonnes (au moins 10)
+		// Pour chaque élément de la boucle on vérifier si il a au moins 10 colonnes
 		if len(record) >= 10 {
-			// Condition pour récupérer les 2 premier caractère (du code postal)
+			// Si la colonne 9 (Code Postale) contient 2 ou plus caractères
 			if len(record[9]) >= 2 {
+				// On récupère le département dans une variable
 				codeDept = strings.TrimSpace(record[9][:2])
+
+				// Boucle pour parcourir la liste des départements à éxtraire
 				for _, dept := range DepartList {
+					// Si le département de l'enregistrement correspond au département à extraire
 					if codeDept == strings.TrimSpace(dept) {
 						// Incrémentation de element
 						ComptElement++
@@ -135,8 +143,12 @@ func ReadCSVFileContentAndExtracter() {
 				continue
 			}
 
+			// On definit le code INSEE de l'enregistrement en cours
 			codeInsee := strings.TrimSpace(record[8]) // 9e colonne
+
+			// Boucle pour parcourir la liste des codes INSEE à éxtraire
 			for _, insee := range InseeList {
+				// Si le code INSEE de l'enregistrement correspond au code INSEE à extraire
 				if codeInsee == strings.TrimSpace(insee) {
 					// Incrémentation de element
 					ComptElement++
--- components/logWriter.go
+++ components/logWriter.go
@@ -7,17 +7,23 @@ import (
 )
 
 func LogWriter() {
-	//Initialisation d'une variable now
+	// Initialisation d'une variable now
 	now := time.Now()
 
-	// Ajout des informations aux fichier log
+	// Initilisation de la variable qui contient le chemin du fichier log
 	logFilePath := os.Getenv("LOG_FILE")
+	// Ouverture du fichier log ou création
 	logFile, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Printf("Une erreur s'est produite lors de l'ouverture du fichier log: %v", err)
 		return
 	}
+	// Fermeture du fichier
 	defer logFile.Close()
 
-	logFile.WriteString(fmt.Sprintf("\nBLABLACAR Une extraction a été effectuée le : %s | depuis le fichier source %s | vers nouveau fichier %v | les filtres actifs sont INSEE : %v et DPT :%v \n Il y a %v élements totaux dans le fichier source, sur tous ces éléments, il y'a %v éléments exportés. \n----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------", now.Format("2006-01-02 15:04:05"), FilePath, ExtractFilePath+CompleteExtractFileName+".csv", CityINSEE, DepartID, ComptTotal, ComptElement))
+	// Variable qui contien le message
+	message := fmt.Sprintf("Une extraction a été effectuée le : %s | depuis le fichier source %s | vers nouveau fichier %v | les filtres actifs sont INSEE : %v et DPT :%v \n Il y a %v élements totaux dans le fichier source, sur tous ces éléments, il y'a %v éléments exportés. \n----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------", now.Format("2006-01-02 15:04:05"), FilePath, ExtractFilePath+CompleteExtractFileName+".csv", CityINSEE, DepartID, ComptTotal, ComptElement)
+
+	// Ecriture dans le Log
+	logFile.WriteString(message)
 }
